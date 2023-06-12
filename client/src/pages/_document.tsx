80f7b93db9c0e8439c222a93907f0570edbac0b6
hoangbao0201 <hoangbao020103@gmail.com>
2023-06-12 21:39:48 +0700

    Client: Add google ads

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -4,12 +4,17 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700;900&display=swap"
-                rel="stylesheet"
-            />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700;900&display=swap"
+                    rel="stylesheet"
+                />
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6688547661590907"
+                    crossOrigin="anonymous"
+                ></script>
             </Head>
             <body>
                 <Main />
