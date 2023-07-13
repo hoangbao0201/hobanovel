966bb17a9562e52c0f56760962a0d08359057f32
Nguyễn Hoàng Bảo <hoangbao020103@gmail.com>
2023-07-13 12:34:17 +0700

    Update server-sitemap.xml.ts

--- client/src/pages/server-sitemap.xml.ts
+++ client/src/pages/server-sitemap.xml.ts
@@ -7,36 +7,10 @@ const URL = "https://hobanovel.online";
  
 function generateSiteMap(novels : any) {
 
-    // console.log(`<url>
-    //     <loc>${URL}/truyen/${novels[1].slug}</loc>
-    //     <lastmod>${novels[1].updatedAt}</lastmod>
-
-    //     <changefreq>Thường xuyên</changefreq>
-    //     <priority>0.8</priority>
-
-    //     <image:image>
-    //     <image:loc>${novels[1].thumbnailUrl}</image:loc>
-    //     <image:caption>hobanovel - ${novels[1].title}</image:caption>
-    //     </image:image>
-
-    //     <news:news>
-    //     <news:publication>
-    //     <news:name>hobanovel</news:name>
-    //     <news:language>Việt Nam</news:language>
-    //     </news:publication>
-    //     <news:publication_date>${novels[1].createdAt}</news:publication_date>
-    //     <news:title>${novels[1].title}</news:title>
-    //     <news:keywords>hobanovel ${novels[1].title}</news:keywords>
-    // </url>`)
-
     return (
         `<?xml version="1.0" encoding="UTF-8"?>
             <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
                 <!-- Add the static URLs manually -->
-
-            <url>
-                <loc>${URL}</loc>
-            </url>
 
             ${novels.map((novel : any) => {
                 return (
@@ -57,11 +31,9 @@ function generateSiteMap(novels : any) {
 export const getServerSideProps : GetServerSideProps = async ({ res } : GetServerSidePropsContext) => {
     const dataRes = await getAllNovelForSeoHandle();
 
-    // Generate the XML sitemap with the blog data
     const sitemap = generateSiteMap(dataRes);
 
     res.setHeader("Content-Type", "text/xml");
-    // Send the XML to the browser
     res.write(sitemap);
     res.end();
 
